4edde86d70ceb2b46bb058a4cffdbb60809f951c
Teja Dasari <ichorlifephoenix@gmail.com>
2025-10-22 16:54:36 -0700

    first commit

---
+++ front-end/src/pages/landing.tsx
@@ -0,0 +1,22 @@
+
+
+
+function LandingPage() {
+  return (
+    <>
+        <div>
+            <h1>This is the Landing Page</h1>
+        </div>
+        <div>
+            <nav>
+                <a href="/login">Login</a>
+                <a href="/register">Register</a>
+                <a href="/about">About</a>
+                <a href="/contact">Contact</a>
+            </nav>
+        </div>
+    </>
+  )
+}
+
+export default LandingPage;
