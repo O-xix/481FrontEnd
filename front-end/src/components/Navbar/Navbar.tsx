e5f529a8eba5a70b18391b24bbe2b36fede73485
Quashnock <awq@uw.edu>
2025-11-03 15:01:30 -0800

    Added documentation.

--- front-end/src/components/Navbar/Navbar.tsx
+++ front-end/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,8 @@
 import './Navbar.css';
-
+/**
+ * Navbar component to be used across pages for navigation.
+ */
 function Navbar(){
-
     return (
         <header id="navbar-container">
             <h1 id="navbar-title">Car Collision Heatmap</h1>
--- front-end/src/components/Popup/Popup.tsx
+++ front-end/src/components/Popup/Popup.tsx
@@ -1,5 +1,9 @@
 import './Popup.css';
 
+/**
+ * Popups that appear when a user clicks a feature.
+ * @param {string} stateName - The name of the state being clicked on.
+ */
 function Popup({stateName, value}: {stateName: string, value: number}){
     return (<div id = "popup-container" style={{ padding: '8px' }}>
         <h3 id="popup-title">{stateName}</h3>
